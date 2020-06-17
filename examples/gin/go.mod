c7f29e3d00badc8b1a24eca268c35064883edb32
Alex P <alexeek@protonmail.com>
2020-06-17 07:42:50 +0100

    Make examples more clear and comment

--- examples/gin/go.mod
+++ examples/gin/go.mod
@@ -5,5 +5,13 @@ go 1.14
 require (
 	github.com/fjah/luapi v0.0.0-20200617061809-ebca925c206e
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/yuin/gopher-lua v0.0.0-20200603152657-dc2b0ca8b37e
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- examples/gin/main.go
+++ examples/gin/main.go
@@ -8,6 +8,8 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
+// We need to implement the Router and Context interfaces of LuAPI. This means that the library can essentially
+// support all routers.
 type ginRouter struct {
 	engine *gin.Engine
 }
@@ -16,6 +18,7 @@ type ginContext struct {
 	original *gin.Context
 }
 
+// Body should just return a RequestBody of the parsed application/json data.
 func (c ginContext) Body() luapi.RequestBody {
 	req := luapi.RequestBody{}
 	if err := c.original.BindJSON(&req); err != nil {
@@ -24,12 +27,16 @@ func (c ginContext) Body() luapi.RequestBody {
 	return req
 }
 
+// Respond should send an application/json response with the status code as Status and the body as the struct.
+// The ResponseBody struct already has JSON tags, so there's no need to do that yourself.
 func (c ginContext) Respond(resp luapi.ResponseBody) {
 	c.original.JSON(resp.Status, resp)
 }
 
+// POST simply allows LuAPI to register POST requests.
 func (r ginRouter) POST(path string, handler func(luapi.Context)) {
 	ginHandler := func(c *gin.Context) {
+		// We need to make sure to instantiate a ginContext and call handler here.
 		ctx := ginContext{original: c}
 		handler(ctx)
 	}
@@ -38,14 +45,25 @@ func (r ginRouter) POST(path string, handler func(luapi.Context)) {
 
 func main() {
 	router := gin.New()
+	// Once we're done, just instantiate LuAPI with our router.
 	api := luapi.New(ginRouter{engine: router})
 
+	// The global handler is the one used when a namespace isn't provided. Namespaces are essentially way
+	// to virtualise endpoints; they can have a set amount of functions, etc.
 	api.Handlers["global"] = luapi.Handlers{
+		// Req is called when a request is made to the server.
 		Req: func(l *lua.LState, script string) error {
+			// This simply tells the Lua state to execute the script. Note that the bootstrapper has already
+			// been called.
 			return l.DoString(script)
 		},
+		// Once Req finishes, Res' *closure* will be called.
 		Res: func(c luapi.Context) lua.LGFunction {
+			// Lets return a LGFunction; this lets us access variables passed in Lua.
 			return func(state *lua.LState) int {
+				// Just respond to the request with the first parameter passed to the `respond` Lua function as a
+				// string. You can also push (return) values with state.Push; make sure to update `return 0` to be
+				// the amount of returned values.
 				c.Respond(luapi.ResponseBody{
 					Status: http.StatusOK,
 					Body:   state.ToString(1),
@@ -54,6 +72,7 @@ func main() {
 			}
 		},
 	}
+	// Set up the API, running a test.
 	api.Setup(true)
 
 	router.Run(":80")
