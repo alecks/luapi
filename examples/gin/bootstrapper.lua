e4a2acc9204dafc75a3f2264e0c0361424357966
Alex P <alexeek@protonmail.com>
2020-06-17 08:36:46 +0100

    Fix typo

--- examples/gin/bootstrapper.lua
+++ examples/gin/bootstrapper.lua
@@ -1,6 +1,6 @@
 --[[
     bootstrapper.lua is responsible for bootstrapping all incoming scripts.
-    This should include — for example — the creation a sandbox, and possibly global functions/variables.
+    This should include — for example — the creation of a sandbox, and possibly global functions/variables.
     To create namespace-specific funcs/vars, all you have to do is run DoString/File twice.
 --]]
 
